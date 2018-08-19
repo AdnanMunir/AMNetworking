3af4cfbc6374c343de406f1cdd81868d1dfdf623
Attiq <attiq.ur.rehman1991@gmail.com>
2018-08-19 12:15:53 +0500

    Initial Commit

---
+++ AMNetworking/AMNetworking.h
@@ -0,0 +1,19 @@
+//
+//  AMNetworking.h
+//  AMNetworking
+//
+//  Created by Attiq on 8/19/18.
+//  Copyright © 2018 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMNetworking.
+FOUNDATION_EXPORT double AMNetworkingVersionNumber;
+
+//! Project version string for AMNetworking.
+FOUNDATION_EXPORT const unsigned char AMNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMNetworking/PublicHeader.h>
+
+
